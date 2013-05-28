176970aa31091bea355f8d2c7474d983d271a5a3
AgelxNash <iam@agel-nash.ru>
2013-05-29 03:30:33 +0600

    Загрузка проекта

---
+++ core/components/qrcode/elements/snippets/snippet.qrcode.php
@@ -0,0 +1,97 @@
+<?php
+/**
+* QRCode
+* Генератор QR кодов
+*
+* Copyright 2013 by Agel_Nash <Agel_Nash@xaker.ru>
+*
+* @category content
+* @license GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
+* @author Agel_Nash <Agel_Nash@xaker.ru>
+* @date 29.05.2012
+* @version 1.0
+*
+*/
+if(empty($modx) || !($modx instanceof modX)) return '';
+
+$modelPath = (string)$modx->getOption('qrcode.core_path',null,$modx->getOption('core_path').'components/qrcode/').'model/';
+
+$config=array();
+$config['input']=(string)$modx->getOption('input',$scriptProperties);  //Кодируемая строка
+
+$config['method'] = (string)$modx->getOption('method',$scriptProperties,'png'); //Метод конвертации png, svg, text, eps
+$config['mode'] = (string)$modx->getOption('mode',$scriptProperties,'L'); //Режим корректности ошибок. L - 7%, M - 15%, Q - 25%, H - 30%
+$config['padding'] = intval($modx->getOption('padding',$scriptProperties,'2')); // размер белой рамки вокруг кода
+$config['dot'] = min(max(intval($modx->getOption('dot',$scriptProperties,'4')),1),10); //1...10 размер каждого квадрата в коде (в px).
+$config['replace'] = intval($modx->getOption('replace',$scriptProperties,'0')); //Замена файла
+$config['folder'] = trim((string)$modx->getOption('folder',$scriptProperties,'QRcode'),'/'); //Папка куда сохранять картинку
+$config['filename'] = (string)$modx->getOption('filename',$scriptProperties,sha1($config['input'])); //Имя файла куда сохранять картинку
+
+/*
+//for Evolutuion
+if(isset($options)){
+    $options=explode("&",$options);
+    foreach ($options as $value) {
+        $params = explode("=", $value,2);
+        if(isset($config[$params[0]])){
+            $config[$params[0]] = $params[1];
+        }
+        
+    }
+}*/
+$flag = false;
+switch(true){
+    case ($config['input']==''): {
+        $modx->log(modX::LOG_LEVEL_DEBUG,'[QRCode] text is empty, aborting.'); 
+		break;
+	}
+	case (!in_array($config['mode'],array('L','M','Q','H'))): {
+		$modx->log(modX::LOG_LEVEL_ERROR,'[QRCode] unknown mode'); 
+		break;
+	}
+	case (!file_exists($modelPath.'qrcode/phpqrcode.php')):{
+		$modx->log(modX::LOG_LEVEL_DEBUG,'[QRCode] file with class QRCode not found');
+		break;
+	}
+	case (!class_exists('QRcode',false)):{
+		include_once($modelPath.'qrcode/phpqrcode.php');
+		if(class_exists('QRcode',false)){
+			$flag = true;
+		}
+		break;
+	}
+	case (!method_exists('QRcode',$config['method'])):{
+		$modx->log(modX::LOG_LEVEL_ERROR,'[QRCode] unknown method QRcode generate'); 
+		break;
+	}
+	default:{
+		$flag = true;
+	}
+}
+
+if($flag){
+    $out = $config['folder'].'/'.$config['filename'].'.'.$config['method'];
+    $fullpath = $modx->getOption('assets_path').$out;
+    
+    $modx->getService('fileHandler','modFileHandler');
+    $dir = $modx->fileHandler->make($fullpath,array(),'modDirectory');
+    if(!is_object($dir) || !($dir instanceof modDirectory)) {
+        $modx->log(modX::LOG_LEVEL_ERROR,'[QRCode] not a directory');
+    }
+    $dir->create();
+    
+    if($config['replace'] || (!$config['replace'] && !file_exists($fullpath))){
+        QRcode::$config['method'](
+        	$config['input'],
+    		$fullpath,
+    		$config['mode'],
+    		$config['dot'],
+    		$config['padding']
+    	);
+        
+    }
+}else{
+	$out = '';
+}
+
+return $out;
---
+++ core/components/qrcode/lexicon/ru/properties.inc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* QRCode
+* Генератор QR кодов
+*
+* Copyright 2013 by Agel_Nash <Agel_Nash@xaker.ru>
+*
+* @category content
+* @license GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
+* @author Agel_Nash <Agel_Nash@xaker.ru>
+* @date 29.05.2012
+* @version 1.0
+*
+*/
+/**
+ * Russian Lexicon for QRcode
+ *
+ * @package QRcode
+ *
+ * @subpackage lexicon
+ * @language ru
+ */
+
+$_lang['qrcode.input'] = 'Информация, которую нужно закодировать';
+$_lang['qrcode.method'] = 'Метод конвертации';
+$_lang['qrcode.mode'] = 'Режим корректности ошибок';
+$_lang['qrcode.padding'] = 'Размер белой рамки вокруг кода';
+$_lang['qrcode.dot'] = 'Размер квадрата в коде';
+$_lang['qrcode.folder'] = 'Папка из публичной директории assets, куда складывать QR коды';
+$_lang['qrcode.filename'] = 'Новое имя файла';
+$_lang['qrcode.replace'] = 'Обновлять ли QR-код при совпадении имен';
